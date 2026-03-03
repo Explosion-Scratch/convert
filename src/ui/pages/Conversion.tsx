9873107b420fad2cb0c39d69837f4d8c95bb52b0
Markus <65797058+headquarter8302@users.noreply.github.com>
2026-03-03 03:33:36 -0500

    Run through formatter

--- src/ui/pages/Conversion.tsx
+++ src/ui/pages/Conversion.tsx
@@ -13,8 +13,8 @@ import SelectedFileInfo from "../components/Conversion/SelectedFileInfo";
 import ConversionHeader from "../components/Conversion/ConversionHeader";
 import { AllOptions } from 'src/main.new';
 
-import FormatExplorer, {type FormatTypeCard} from "../components/Conversion/FormatExplorer.tsx";
-import {useState} from "preact/hooks";
+import FormatExplorer, { type FormatTypeCard } from "../components/Conversion/FormatExplorer.tsx";
+import { useState } from "preact/hooks";
 
 interface ConversionPageProps {
 
@@ -22,14 +22,14 @@ interface ConversionPageProps {
 
 const sidebarItems: FormatCategory[] = [ // Placeholder categories
     { id: "arc", category: "Archive", icon: faBoxArchiveSolid },
-    { id: "img", category: "Image", icon: faImageRegular},
+    { id: "img", category: "Image", icon: faImageRegular },
     { id: "doc", category: "Document", icon: faFileLinesRegular },
     { id: "vid", category: "Video", icon: faVideoSolid },
     { id: "aud", category: "Audio", icon: faMusicSolid },
     { id: "ebk", category: "E-Book", icon: faFileLinesRegular },
 ];
 
-export default function Conversion(props: ConversionPageProps | undefined) {
+export default function Conversion({ }: ConversionPageProps) {
     const AvailableConversionFormats: FormatTypeCard[] = getConversionFormats();
     const [selectedFormat, setSelectedFormat] = useState<FormatTypeCard | null>(null);
 
@@ -57,7 +57,7 @@ export default function Conversion(props: ConversionPageProps | undefined) {
             <SelectedFileInfo className="mobile-only" />
 
             <main className="conversion-main">
-                <FormatExplorer categories={sidebarItems} conversionFormats={AvailableConversionFormats} onSelect={setSelectedFormat}/>
+                <FormatExplorer categories={ sidebarItems } conversionFormats={ AvailableConversionFormats } onSelect={ setSelectedFormat } />
 
                 {/* Right Settings Sidebar / Bottom Settings Accordion */ }
                 <aside className="settings-sidebar">
